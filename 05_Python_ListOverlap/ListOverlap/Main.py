4c238aeb38bb22beb9b4c3e5432d56ab730037c3
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2017-10-19 23:51:00 -0500

    Python Practice 05 Updated

---
+++ 05_Python_ListOverlap/ListOverlap/Main.py
@@ -0,0 +1,23 @@
+"""
+List Overlap
+
+Exercise 5
+Take two lists, say for example these two:
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+and write a program that returns a list that contains only elements that are common between the lists
+(without duplicates). Make sure your program works on two lists of different sizes.
+
+Extras
+1.- Randomly generate two lists to test this.
+2.- Write this in one line of Python (Don't worry if you can't figure this out at this point
+    (We'll get to it soon).
+"""
+
+"""
+Solution coded by: KriztyanPM
+Date: 10/19/2017 [19/10/2017 for Mexico]
+Exercise from: http://www.practicepython.org/exercise/2014/03/05/05-list-overlap.html
+"""
