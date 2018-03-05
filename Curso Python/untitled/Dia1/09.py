37f6578f309391411b2eb8a952b8a5cdcd31b6ff
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2018-03-05 15:59:33 -0600

    New excercises added

---
+++ Curso Python/untitled/Dia1/09.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+
+#Primer comentario del código
+Hola = 1    #Segundo comentario del código
+            #Tercer comentario del código
+cadena = "#Este ya no es un comentario, solamente una cadena"
+
+print(str(Hola))
+print(cadena)
+#Fin del código
