6016d8adcd08b33fbd423d30451f20fe9eabfced
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2018-03-22 23:12:27 -0600

    Excercise 01 updated and finished

--- Curso Python/untitled/Dia1/Ejercicio_01.py
+++ Curso Python/untitled/Dia1/Ejercicio_01.py
@@ -17,16 +17,20 @@ Almacenar los datos de una persona
 Validar su CURP, y de ser válido, calcular su IMC (Índice de Masa Corporal
 y mostrar la información de la persona
 """
+"""
+    En esta primera parte, se almacenan los datos en diferentes variables, que serán
+    utilizadas conforme sean necesitadas.
+"""
 
 #Definición de variables a utilizar
-
+"""
 Nombre = ""         #Sólo un nombre
 ApellidoP = ""      #Apellido Paterno
 ApellidoM = ""      #Apellido Materno
 Edad = 0            #Edad del usuario
 Peso = 0.0          #Peso en Kilogramos
 Estatura = 0.0        #Estatura en Metros
-#CURP = ""           #Clave Única de Registro Poblacional
+"""
 
 #Solicitud de los datos del Usuario
 """
@@ -36,31 +40,25 @@ ApellidoM = raw_input("Ingrese su apellido Materno: ")
 Edad = int(raw_input("Ingrese su edad: "))
 Peso = float(raw_input("Ingrese su peso (Kg): "))
 Estatura = float(raw_input("Ingrese su estatura (m): "))
-"""
+
 print("Para validar sus datos, ingrese su DNI: ")
 DNI = raw_input("DNI: ")
+"""
 
-def valDNI(p_DNI):
-    letrasDNI = ("T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E")
-    #Determinar si el DNI es de ciudadano Español o Residente Extranjero
-    tempNIF = p_DNI[1:len(p_DNI)]
-    NIE = ["X", "Y", "Z"]
-    if p_DNI[0] == "X":
-        p_DNI = str(0)+tempNIF
-    elif p_DNI[0] == "Y":
-        p_DNI = str(1) + tempNIF
-    elif p_DNI[0] == "Z":
-        p_DNI = str(2) + tempNIF
-    #Si no hay letra X, Y o Z al inicio, el DNI pertenece a ciudadano Español
-    numDNI = int(p_DNI[0:len(p_DNI)-1])
-    letraDNI = p_DNI[8]
-    calcLetraDNI = numDNI % 23
-    if letraDNI == letrasDNI[calcLetraDNI]:
-        print("La DNI fue ingresada y validada correctamente")
-    else:
-        print("La DNI ingresada contiene un error, intente nuevamente")
+"""
+    En esta segunda parte se almacenan los datos en un diccionario
+"""
 
-valDNI(DNI)
+#Definición del diccionario y solicitud de datos del usuario
+persona = {}
+persona["Nombre"] = raw_input("Ingrese su nombre (Solo el primer nombre): ")
+persona["ApellidoP"] = raw_input("Ingrese su apellido Paterno: ")
+persona["ApellidoM"] = raw_input("Ingrese su apellido Materno: ")
+persona["Edad"] = int(raw_input("Ingrese su edad: "))
+persona["Peso"] = float(raw_input("Ingrese su peso (Kg): "))
+persona["Estatura"] = float(raw_input("Ingrese su estatura (m): "))
+print("Para validar sus datos, ingrese su DNI: ")
+persona["DNI"] = raw_input("DNI: ")
 
 
 #Cálculo del Índice de Masa Corporal
@@ -80,8 +78,48 @@ def CalcIMC(p_Peso, p_Est):
         else:
             print("--Su peso es elevado")
 #Fin de la función para calcular IMC
+
+#Función para imprimir datos de usuario dede Diccionario
+def printUsr():
+    print("------------------------------------------------")
+    print("El usuario " + persona["Nombre"] + " " + persona["ApellidoP"] + " " + persona["ApellidoM"])
+    print("de " + str(persona["Edad"]) + " años.")
+    print("Peso: " + str(persona["Peso"]) + " Kg")
+    print("Estatura: " + str(persona["Estatura"]) + " mts")
+    CalcIMC(persona["Peso"], persona["Estatura"])
+    print("------------------------------------------------")
+
 """
-#Mostrar datos ingresados del usuario
+    Definición de una función para validación de DNI, de acuerdo al algoritmo obtenido mediante
+    el siguiente enlace:
+    http://www.interior.gob.es/web/servicios-al-ciudadano/dni/calculo-del-digito-de-control-del-nif-nie
+"""
+def valDNI(p_DNI):
+    letrasDNI = ("T","R","W","A","G","M","Y","F","P","D","X","B","N","J","Z","S","Q","V","H","L","C","K","E")
+    #Determinar si el DNI es de ciudadano Español o Residente Extranjero
+    tempNIF = p_DNI[1:len(p_DNI)]
+    NIE = ["X", "Y", "Z"]
+    if p_DNI[0] == "X":
+        p_DNI = str(0)+tempNIF
+    elif p_DNI[0] == "Y":
+        p_DNI = str(1) + tempNIF
+    elif p_DNI[0] == "Z":
+        p_DNI = str(2) + tempNIF
+    #Si no hay letra X, Y o Z al inicio, el DNI pertenece a ciudadano Español
+    numDNI = int(p_DNI[0:len(p_DNI)-1])
+    letraDNI = p_DNI[8]
+    calcLetraDNI = numDNI % 23
+    if letraDNI == letrasDNI[calcLetraDNI]:
+        print("La DNI fue ingresada y validada correctamente")
+        print("Mostrando los datos del usuario...")
+        printUsr()
+    else:
+        print("La DNI ingresada contiene un error, intente nuevamente")
+
+valDNI(persona["DNI"])
+
+"""
+#Mostrar datos ingresados del usuario (Mediante las variables)
 print("------------------------------------------------")
 print("El usuario "+Nombre+" "+ApellidoP+" "+ApellidoM)
 print("de "+str(Edad)+" años.")
