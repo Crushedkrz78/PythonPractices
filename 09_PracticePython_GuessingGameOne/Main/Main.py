27b52f9a9353c34cab0905318676975f0f1c3c61
Crushedkrz78 <matrix_kriztyan@live.com.mx>
2018-08-13 17:09:23 -0500

    Just an update of my repository, I'll start working on when I'm ready to

--- 09_PracticePython_GuessingGameOne/Main/Main.py
+++ 09_PracticePython_GuessingGameOne/Main/Main.py
@@ -10,3 +10,8 @@ Extras:
  - Keep track of how many guesses the user has taken, and when the game ends,
    print this out.
 """
+text =  "This message is only to test my Ubuntu Python Enviroment\n" \
+      "this version will just contain the message and then I'll update to \n" \
+      "a working code of this exercise. Tschuss!!"
+
+print (text)
