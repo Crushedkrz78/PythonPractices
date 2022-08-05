c7aebb8e0584bb306f90bdafaca35c47f7f9b3dc
Crushedkrz78 <crispmolinito93@gmail.com>
2022-08-05 17:32:22 -0500

    Updated Script Header and intoduction

--- data_structures/arrays/array_2d_ds.py
+++ data_structures/arrays/array_2d_ds.py
@@ -1,10 +1,7 @@
-# print("Hola Mundo")
-
-#En este ejemplo renovado, se estará trabajando en un ambiente Linux virtualizado para no contaminar el ambiente local del equipo
-
-# print("Hola Mundo desde Ubuntu 22.04")
-
-# print("Segunda prueba del Hola Mundo en Ubuntu")
+"""
+En este ejercicio se han incluido algunas mejoras en el manejo de mi còdigo en Python.
+Estos ejercicios se estàn realizandio a través del material de entrenamiento de HackerRank.
+"""
 
 import math
 import os
